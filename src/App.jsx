b5ec6edf6990862d7752367884cf0618ae774b95
NavyaPodili <160600065+NavyaPodili@users.noreply.github.com>
2024-05-20 16:58:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import CounterApp from './Components/CounterApp';
+
+function App() {
+    return (
+        <div className="App">
+            <CounterApp />
+        </div>
+    );
+}
+
+export default App;
